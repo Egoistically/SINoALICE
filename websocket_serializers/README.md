480d91763f96a944b16d476294811bd77e053d5b
Egoist <298qjm982374@protonmail.com>
2021-05-29 00:18:55 +0200

    4

---
+++ websocket_serializers/README.md
@@ -0,0 +1,9 @@
+# Websocket Serializer
+Python serializer for Quests' `SystemScheduleInfo` event. Can be integrated with my [websocket script](https://github.com/Egoistically/SINoALICE/blob/master/mitmproxy/mitm_websocket.py) for mitmproxy.  
+  
+Disclaimer:
+* Haven't checked thoroughly for possible type errors/improvements.
+* The game uses the same sort of hardcoded serialization, therefore, a future update may break it.
+* Not responsible for its use and or misuse, do what you want with it.
+  
+Made and tested on Python `3.7.9`.
---
+++ websocket_serializers/quests_serializer.py
@@ -0,0 +1,241 @@
+from dataclasses import dataclass
+from inspect import signature
+from typing import List
+
+@dataclass()
+class questData:
+    currentWaveNum: int
+    currentQuestWaveMstId: int
+    isRare: bool
+    battleStatus: int
+    result: bool
+    continueCount: int
+
+@dataclass()
+class general:
+    timestamp: int
+
+@dataclass()
+class guild:
+    guildDataId: int
+    comboCount: int
+    comboCurrentUserId: int
+    lethalArtCount: int
+
+@dataclass()
+class member:
+    userId: int
+    guildDataId: int
+    modeType: int
+    hp: int
+    questSp: int
+    maxHp: int
+    questSpMax: int
+    isNoDamage: bool
+    isAi: bool
+    isAuto: bool
+    attackEffectLevel: int
+    defenceEffectLevel: int
+    magicAttackEffectLevel: int
+    magicDefenceEffectLevel: int
+    enemyTargetMemberRank: int
+    ownTargetMemberRank: int
+
+@dataclass()
+class lethalArt:
+    lethalArtMstId: int
+    artCount: int
+    successArtMstId: int
+    failureArtMstId: int
+    lethalArtMethodMstId: int
+    status: int
+    needCount: int
+    startPreEffectTime: int
+    startPrepareTime: int
+    endPrepareTime: int
+    winGuildDataId: int
+    startTime: int
+    endTime: int
+
+@dataclass()
+class art:
+    artMstId: int
+    guildDataId: int
+    name: str
+    startTime: int
+    endTime: int
+    initiator: int
+
+@dataclass()
+class historyInfluenceData:
+    questHistoryId: int
+    questData: questData
+    general: general
+    guilds: List[guild]
+    members: List[member]
+    lethalArt: lethalArt
+    art: List[art]
+
+@dataclass()
+class userActInfluence:
+    questHistoryId: int
+    actionUserId: int
+    actionGuildDataId: int
+    receivedUserId: int
+    receivedGuildDataId: int
+    actionType: int
+    usedSp: int
+    cardMstId: int
+    skillMstId: int
+    attackType: int
+    modeChange: int
+    killed: int
+    actionFlag: int
+    actionValue: int
+
+@dataclass()
+class modeChangeMember:
+    userId: int
+    name: str
+    guildDataId: int
+    roleType: int
+    memberRank: int
+    hp: int
+    questSp: int
+    attack: int
+    defence: int
+    magicAttack: int
+    magicDefence: int
+    maxHp: int
+    questEnemyMstId: int
+    modeType: int
+    maxModeChangeNum: int
+
+@dataclass()
+class userActRes:
+    userActInfluence: List[userActInfluence]
+    modeChangeMember: List[modeChangeMember]
+
+@dataclass()
+class artInfluence:
+    artMstId: int
+    guildDataId: int
+    artActionFlag: int
+    name: str
+    startTime: int
+    endTime: int
+    questHistoryId: int
+    actionUserId: int
+    receivedUserId: int
+    receivedGuildDataId: int
+    actionFlag: int
+    actionValue: int
+
+@dataclass()
+class startArtData:
+    artInfluence: List[artInfluence]
+    modeChangeMember: List[modeChangeMember]
+
+@dataclass()
+class lethalArtInfluence:
+    artMstId: int
+    guildDataId: int
+    lethalArtActionFlag: int
+    name: str
+    startTime: int
+    endTime: int
+    questHistoryId: int
+    actionUserId: int
+    receivedUserId: int
+    receivedGuildDataId: int
+    actionFlag: int
+    actionValue: int
+
+@dataclass()
+class startLethalArtData:
+    lethalArtInfluence: List[lethalArtInfluence]
+    modeChangeMember: List[modeChangeMember]
+
+class QuestSystemScheduleInfo():
+
+    def __init__(self, _list: list) -> None:
+        self._list = _list
+        self._index = 0
+        self.historyInfluenceData = historyInfluenceData(0, 0, 0, [], [], 0, [])
+        self.userActRes = userActRes([], [])
+        self.startArtData = startArtData([], [])
+        self.startLethalArtData = startLethalArtData([], [])
+        self.serialize_list()
+
+    def read(self) -> int:
+        c = self._list[self._index]
+        self._index += 1
+        return c
+
+    def check(self) -> bool:
+        return self.read()
+
+    def get_args(self, _class: type):
+        return [self.read() for _ in range(len(signature(_class).parameters))]
+
+    def serialize_historyInfluence(self) -> None:
+        if self.check():
+            self.historyInfluenceData.questHistoryId = self.read()
+
+            if self.check():
+                self.historyInfluenceData.questData = questData(*self.get_args(questData))
+
+            if self.check():
+                self.historyInfluenceData.general = general(self.read())
+
+            for _ in range(self.read()):
+                if self.check():
+                    self.historyInfluenceData.guilds.append(guild(*self.get_args(guild)))
+
+            for _ in range(self.read()):
+                if self.check():
+                    self.historyInfluenceData.members.append(member(*self.get_args(member)))
+
+            if self.check():
+                self.historyInfluenceData.lethalArt = lethalArt(*self.get_args(lethalArt))
+
+            for _ in range(self.check()):
+                if self.check():
+                    self.historyInfluenceData.art.append(art(*self.get_args(art)))
+
+    def serialize_userActRes(self) -> None:
+        if self.check():
+            for _ in range(self.read()):
+                if self.check():
+                    self.userActRes.userActInfluence.append(userActInfluence(*self.get_args(userActInfluence)))
+
+            for _ in range(self.read()):
+                if self.check():
+                    self.userActRes.modeChangeMember.append(modeChangeMember(*self.get_args(modeChangeMember)))
+
+    def serialize_startArtData(self) -> None:
+        if self.check():
+            for _ in range(self.read()):
+                if self.check():
+                    self.startArtData.artInfluence.append(artInfluence(*self.get_args(artInfluence)))
+
+            for _ in range(self.read()):
+                if self.check():
+                    self.startArtData.modeChangeMember.append(modeChangeMember(*self.get_args(modeChangeMember)))
+
+    def serialize_startLethalArtData(self) -> None:
+        if self.check():
+            for _ in range(self.read()):
+                if self.check():
+                    self.startArtData.artInfluence.append(lethalArtInfluence(*self.get_args(lethalArtInfluence)))
+
+            for _ in range(self.read()):
+                if self.check():
+                    self.startArtData.modeChangeMember.append(modeChangeMember(*self.get_args(modeChangeMember)))
+
+    def serialize_list(self) -> None:
+        if self.check():
+            self.serialize_historyInfluence()
+            self.serialize_userActRes()
+            self.serialize_startArtData()
+            self.serialize_startLethalArtData()
